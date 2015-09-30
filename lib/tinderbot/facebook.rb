1355317c8a2b31d22d7c67c2ac1729d1f8330fda
Jack Yeh <jack.yeh@gmail.com>
2015-09-29 20:54:15 -0600

    update browser to phantomjs

--- lib/tinderbot/facebook.rb
+++ lib/tinderbot/facebook.rb
@@ -3,7 +3,7 @@ module Tinderbot
     FACEBOOK_AUTHENTICATION_TOKEN_URL = 'https://www.facebook.com/dialog/oauth?client_id=464891386855067&redirect_uri=https://www.facebook.com/connect/login_success.html&scope=basic_info,email,public_profile,user_about_me,user_activities,user_birthday,user_education_history,user_friends,user_interests,user_likes,user_location,user_photos,user_relationship_details&response_type=token'
 
     def self.get_credentials(facebook_email, facebook_password)
-      browser = Watir::Browser.new
+      browser = Watir::Browser.new :phantomjs
       browser.goto FACEBOOK_AUTHENTICATION_TOKEN_URL
       browser.text_field(id: 'email').when_present.set facebook_email
       browser.text_field(id: 'pass').when_present.set facebook_password
